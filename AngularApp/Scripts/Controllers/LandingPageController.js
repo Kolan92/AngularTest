7af9150ed6d112e62f257438bfe3374d5fcd5377
Kolan <pawel.krzysztof.kolanowski@gmail.com>
2016-02-12 19:08:58 +0100

    EditedMoorse

--- AngularApp/Scripts/Controllers/LandingPageController.js
+++ AngularApp/Scripts/Controllers/LandingPageController.js
@@ -1,6 +1,7 @@
 ﻿var LandingPageController = function($scope) {
     $scope.models = {
-        input: "Hello world"
+        input: "Hello world",
+        output: ""
     };
 
     $scope.translateInput = function () {
@@ -17,9 +18,64 @@
                 }
             }
             output = output + " ";
-            console.log(output);
+            //console.log(output);
         }
+        $scope.models.output = output;
         return output;
+    }
+
+
+    $scope.blinkDiode = function () {
+        console.clear();
+        codeAllSigns(0);
+    }
+
+
+    
+    function codeAllSigns(index) {
+        if ($scope.models.output.length > index) {
+            var daouration;
+            switch ($scope.models.output[index]) {
+                case " ":
+                    daouration = 5000;
+                    break;
+                case "-":
+                    daouration = 3000;
+                    break;
+                case ".":
+                    daouration = 1000;
+                    break;
+                default:
+                    daouration = 1000;
+            }
+            setTimeout(function () {
+                codeCurrentSign($scope.models.output[index]);
+                //TurnOnDiode()
+                codeAllSigns(++index);
+            }, daouration);
+        }
+    }
+
+    function codeCurrentSign(sign) {
+        //switch (sign) {
+        //        case " ":
+        //            blink(5000);
+        //            break;
+        //        case "-":
+        //            blink(3000);
+        //            break;
+        //        case ".":
+        //            blink(1000);
+        //            break;
+        //    default:
+        //}
+        console.log(sign);
+    }
+
+    function blink(daourtion) {
+        setTimeout(function () { $(".led-red").css("background-color", "#ff0000") }, 1000);
+        //$(".led-red").css("background-color", "#ff0000");//background-color: #ff0000;
+        setTimeout(function () { $(".led-red").css("background-color", "#000000") }, 1000);
     }
 
     $scope.dictionary = {
